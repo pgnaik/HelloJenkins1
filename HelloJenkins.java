79cb1471ee95de95b495c94a579571717f317ab5
Dr. Poornima G. Naik <31154391+pgnaik@users.noreply.github.com>
2024-05-27 22:34:57 -0800

    Update HelloJenkins.java

--- HelloJenkins.java
+++ HelloJenkins.java
@@ -1,5 +1,5 @@
 public class HelloJenkins{
   public static void main(String[] args){
-    System.out.println("continuous Integration and Continuous Deployment Using Jenkins...");
+    system.out.println("continuous Integration and Continuous Deployment Using Jenkins...");
   }
 }
